92b360d6addd71653a975bc1d0c90ac36fc65c18
ErikOrtiz4756 <37184254@uaz.edu.mx>
2022-03-12 23:52:07 -0600

    Programa2 inicio

---
+++ Programa2/Modelos/Productos.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa2.Modelos{
+    internal class Productos {
+        private string _id;
+        private string _nombre;
+        private string _precio;
+        private string _ventas;
+        private string _categorias;
+
+        public Productos (string id, string nombre, string precio, string ventas, string categorias){
+            this._id = id;
+            this._nombre = nombre;
+            this._precio = precio;
+            this._ventas =  ventas;
+            this._categorias = categorias;
+        }
+
+        public string id {
+            get{return _id;}
+            set{_id = value;}
+        }
+
+        public string nombre {
+            get{return _nombre;}
+            set{_nombre = value;}
+        }
+
+        public string precio {
+            get{return _precio;}
+            set{_precio = value;}
+        }
+
+        public string ventas {
+            get{return _ventas;}
+            set{_ventas = value;}
+        }
+
+        public string categorias {
+            get{return _categorias;}
+            set{_categorias = value;}
+        }
+
+        public override string ToString(){
+            return $"ID: {id}, Nombre: {nombre}, Precio: {precio}, Ventas {ventas}, Categoria: {categorias}";
+        }
+    }
+}
